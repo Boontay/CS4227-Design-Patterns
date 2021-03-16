188d54cdddaa92e70c4cd352f9c42b0ac9153ca2
Thomas <thomas.moorhead.langley@gmail.com>
2021-03-16 15:37:38 +0000

    Code for the Liskov Substitution rule.

---
+++ src/solidrules/liskov_subsitution_principle/bad/Square.java
@@ -0,0 +1,15 @@
+package solidrules.liskov_subsitution_principle.bad;
+
+public class Square extends Rectangle{
+    @Override
+    public void setHeight(double height) {
+        super.setHeight(height);
+        super.setWidth(height);
+    }
+
+    @Override
+    public void setWidth(double width) {
+        super.setHeight(width);
+        super.setWidth(width);
+    }
+}
---
+++ src/solidrules/liskov_subsitution_principle/good/Square.java
@@ -0,0 +1,8 @@
+package solidrules.liskov_subsitution_principle.good;
+
+
+public class Square extends Rectangle{
+    public Square(double side) {
+        super(side, side);
+    }
+}
